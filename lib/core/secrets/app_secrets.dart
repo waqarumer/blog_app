5040a2a8b9163795bc784f55715f72805f638713
waqar umer <waqarkhan864@gmail.com>
2024-04-04 03:32:32 +0500

    blog_app

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://sbmcigyickbzjkegseay.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNibWNpZ3lpY2tiemprZWdzZWF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwOTExMTEsImV4cCI6MjAyNzY2NzExMX0.DliCik2eOIG2qA-ABl2wqKsjr-ehpiTvAV8DQTTk20E';
+}
---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:blog_app/core/error/failures.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signupWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+  
+}
