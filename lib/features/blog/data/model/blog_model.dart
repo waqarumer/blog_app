5b70802eebf7aa5968ed4117293837a7d5e3e6d0
waqar umer <waqarkhan864@gmail.com>
2024-04-08 02:05:36 +0500

    upload blog method

--- lib/features/blog/data/model/blog_model.dart
+++ lib/features/blog/data/model/blog_model.dart
@@ -11,18 +11,19 @@ class BlogModel extends Blog {
     required super.updatedAt,
   });
 
-  Map<String, dynamic>toJson(){
-    return <String,dynamic>{
-    'id': id,
-    'poster_id': posterId,
-    'title': title,
-    'content': content,
-    'image_url': imageUrl,
-    'topics': topics,
-    'updated_at': updatedAt.toIso8601String(),
-  };
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'poster_id': posterId,
+      'title': title,
+      'content': content,
+      'image_url': imageUrl,
+      'topics': topics,
+      'updated_at': updatedAt.toIso8601String(),
+    };
   }
-  factory BlogModel.fromJson(Map<String,dynamic>map){
+
+  factory BlogModel.fromJson(Map<String, dynamic> map) {
     return BlogModel(
       id: map['id'] as String,
       posterId: map['poster_id'] as String,
@@ -30,8 +31,28 @@ class BlogModel extends Blog {
       content: map['content'] as String,
       imageUrl: map['image_url'] as String,
       topics: List<String>.from(map['topics'] ?? []),
-      updatedAt: map['updated_at'] == null ? DateTime.now() : DateTime.parse(map['updated_at']),
+      updatedAt: map['updated_at'] == null
+          ? DateTime.now()
+          : DateTime.parse(map['updated_at']),
+    );
+  }
+  BlogModel copyWith({
+    String? id,
+    String? posterId,
+    String? title,
+    String? content,
+    String? imageUrl,
+    List<String>? topics,
+    DateTime? updateAt,
+  }) {
+    return BlogModel(
+      id: id ?? this.id,
+      posterId: posterId ?? this.posterId,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      imageUrl: imageUrl ?? this.imageUrl,
+      topics: topics ?? this.topics,
+      updatedAt: updatedAt ?? this.updatedAt,
     );
   }
 }
-
